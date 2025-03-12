a53f714278e600611ae7813473e675c98b0b7dca
Bhagya Dilhara <bhagya.d@sits.lk>
2025-03-12 15:46:38 +0530

    added login page view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';  // Optional, for showing alerts
+import { CheckboxModule } from 'primeng/checkbox';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    DividerModule,
+    InputTextModule,
+    CardModule,
+    ToastModule,
+    CheckboxModule,
+    PasswordModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
